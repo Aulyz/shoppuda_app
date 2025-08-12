a8f0761170c169cbba6a2796d6b9529eebde14b5
Auly <ryul8465@naver.com>
2025-08-12 15:15:20 +0900

    createflutter

--- dashboard/templatetags/dateformat.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def formatdate(value, format_string="%Y-%m-%d"):
-    if value:
-        return value.strftime(format_string)
-    return ""
--- search/apps.py
+++
@@ -1,7 +0,0 @@
-# search/apps.py
-from django.apps import AppConfig
-
-class SearchConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'search'
-    verbose_name = '검색'
